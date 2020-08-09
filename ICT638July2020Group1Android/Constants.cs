b352a6ccc03b85b29d19d0a05ef0bde06a2e7dfb
Ruoqian XU <1812924@nzse.ac.nz>
2020-08-09 17:58:38 +1200

    get some changes!

---
+++ ICT638July2020Group1Android/Constants.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace ICT638July2020Group1Android
+{
+    class Constants
+    {
+        public const string ListenConnectionString = "Endpoint=sb://ruoqianxu.servicebus.windows.net/;SharedAccessKeyName=DefaultFullSharedAccessSignature;SharedAccessKey=GQFrRQ/CJE0SA9gpp1+tB6YW09MvaiLs90r5x54X/AI=";
+        public const string NotificationHubName = "PushNotificationDemoIct638";
+    }
+}
---
+++ ICT638July2020Group1Android/activity/Login.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace ICT638July2020Group1Android.activity
+{
+    [Activity(Label = "Login", MainLauncher = true)]
+
+    public class Login : Activity
+    {
+        private EditText txt_username, txt_pwd;
+        private Button btn_login;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.login);
+
+            txt_username = FindViewById<EditText>(Resource.Id.editusernamelogin);
+            txt_pwd = FindViewById<EditText>(Resource.Id.editpasswordlogin);
+            btn_login = FindViewById<Button>(Resource.Id.loginbtnlogin);
+
+            Button btnloginregister = FindViewById<Button>(Resource.Id.loginregister);
+
+
+            btnloginregister.Click += (sender, e) =>
+            {
+                Intent RegisterIntent = new Intent(this, typeof(Register));
+                StartActivity(RegisterIntent);
+            };
+
+            btn_login.Click += (sender, e) =>
+            {
+                //@insert link
+                var request = HttpWebRequest.Create(string.Format(@"https://localhost:5001/api/users"));
+                request.ContentType = "application/json";
+                request.Method = "GET";
+                using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
+                {
+                    if (response.StatusCode != HttpStatusCode.OK)
+                    {
+                        Console.Out.WriteLine("Error fetching data. Server returned status code: {0}", response.StatusCode);
+                        using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+                        {
+                            var content = reader.ReadToEnd();
+                            if (string.IsNullOrWhiteSpace(content))
+                            {
+                                Console.Out.WriteLine("Response contained empty body...");
+                            }
+                            else
+                            {
+                                Console.Out.WriteLine("Response Body: \r\n {0}", content);
+                            }
+
+                        }
+                    }
+                    else
+                    {
+                        Console.Out.WriteLine("Login successful");
+                        Intent MenuIntent = new Intent(this, typeof(Menu));
+                        StartActivity(MenuIntent);
+                    }
+                    
+                }
+            };
+
+        }
+    }
+}
